e90a98095feeb0425bc637bb2244d080d43d7be7
daniel_dev <drodriguesaar@gmail.com>
2020-11-01 15:22:07 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using DanielDevBot.Bots;
+using DanielDevBot.Hubs;
+using Microsoft.AspNet.SignalR;
+using Microsoft.Owin.Cors;
+using Microsoft.Owin.Hosting;
+using Owin;
+using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace DanielDevBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task.Run(() =>
+            {
+                
+                BotFactory.FabricarBot(BotEnum.Twitch).Start();
+            });
+            Task.Run(() =>
+            {
+                string url = "http://localhost:8080";
+                using (WebApp.Start(url))
+                {
+                    Console.WriteLine("Server running on {0}", url);
+                    Console.ReadLine();                    
+                }
+            });
+            //Console.ReadKey();
+            Process.GetCurrentProcess().WaitForExit();
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin.Cors;
+using Owin;
+
+namespace DanielDevBot
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseCors(CorsOptions.AllowAll);
+            app.MapSignalR();
+        }
+    }
+}
